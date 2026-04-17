78b8c920e2bff60b3913979e03f82bcd3507d04a
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-04-17 21:54:20 +0600

    a loading animation

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 
 const RootLayout = () => {
     const data = useLoaderData();
+    const navigation = useNavigation();
+
+    const isLoading = navigation.state === "loading";
 
     return (
         <div>
-         <Navbar/>
-         <Outlet context={data}/>
-        <Footer/>
+            <Navbar/>
+
+            {isLoading && (
+                <div className="loader-container">
+                    <div className="spinner"></div>
+                </div>
+            )}
+
+            <Outlet context={data}/>
+
+            <Footer/>
         </div>
     );
 };
